8a40fe1a33252be7112e96a2cd27b2514bf21634
Gacho <138148584+YuriGacho@users.noreply.github.com>
2023-07-06 14:37:07 -0300

    Add files via upload

---
+++ aula_alex.java.1.java
@@ -0,0 +1,117 @@
+//EXERCICIOS SIMPLES -- YURI GACHO E SIMEI MONDARDO
+public class aula_alex{
+    public static void main(String[] args) {
+        //Escreva um programa que imprima a mensagem "Meu nome é [seu nome]"
+        System.out.println("Meu nome é yuri");
+        System.out.println("--------------------------------");
+        //Escreva um programa que imprima a mensagem "A soma de 2 + 2 é igual a 4"
+        System.out.println("A soma de 2 + 2 é igual a 4");
+        System.out.println("--------------------------------");
+        //Escreva um programa que imprima uma linha com dez asteriscos (*) consecutivos.
+        System.out.println("**********");
+        System.out.println("--------------------------------");
+        //Desenhe um quadrado vazado utilizando o caractere asterisco (*) com lado de tamanho 5.
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+        System.out.println("--------------------------------");
+        //Desenhe um triângulo utilizando o caractere hashtag (#) com altura de 6 linhas
+        System.out.println("           # ");
+        System.out.println("         #   #");
+        System.out.println("        #      #");
+        System.out.println("       #        #");
+        System.out.println("      #          #");
+        System.out.println("     ##############");
+        System.out.println("--------------------------------");
+        //Desenhe um losango utilizando o caractere ponto (.) com largura de 7 caracteres.
+        System.out.println("           . ");
+        System.out.println("         .   .");
+        System.out.println("        .      .");
+        System.out.println("       .        .");
+        System.out.println("      .          .");
+        System.out.println("     .            .");
+        System.out.println("      .          .");
+        System.out.println("        .      .");
+        System.out.println("          .   .");
+        System.out.println("            .   ");
+        System.out.println("--------------------------------");
+        //Desenhe uma pirâmide utilizando o caractere cifrão ($) com altura de 5 linhas
+        System.out.println("           $ ");
+        System.out.println("         $   $");
+        System.out.println("        $      $");
+        System.out.println("       $        $");
+        System.out.println("      $          $");
+        System.out.println("     $$$$$$$$$$$$$$");
+        System.out.println("--------------------------------");
+        //8. Desenhe uma árvore de Natal utilizando o caractere asterisco (*) com altura de 8 linhas
+        System.out.println("           * ");
+        System.out.println("         *   *");
+        System.out.println("        *      *");
+        System.out.println("       *        *");
+        System.out.println("      *          *");
+        System.out.println("     **************");
+        System.out.println("         *****");
+        System.out.println("         *****");
+        System.out.println("--------------------------------");
+        //Desenhe um coração utilizando o caractere asterisco (*) e o caractere espaço em branco ( ) com altura
+        //de 7 linhas
+        System.out.println("     *****  *****                  " );
+        System.out.println( "   *      *      *               " );
+        System.out.println( "   *             *               ");
+        System.out.println( "    *          *                  ");
+        System.out.println(   "    *       *                 "  );
+        System.out.println(    "     *   *                   " );
+        System.out.println(    "       *                        " );
+        System.out.println("--------------------------------");
+        //Desenhe um círculo utilizando o caractere ponto (.) com raio de 5 caracteres
+        System.out.println("    ....  ");
+        System.out.println("  .      .");
+        System.out.println(" .        .");
+        System.out.println("  .      .");
+        System.out.println("   ......");
+        System.out.println("--------------------------------");
+        //Desenhe uma seta apontando para a direita utilizando o caractere maior que (>) com altura de 5 linhas
+        System.out.println("              >");
+        System.out.println("                >");
+        System.out.println(">>>>>>>>>>>>>>>>>>>");
+        System.out.println("                >");
+        System.out.println("              >");
+        System.out.println("--------------------------------");
+        //Desenhe uma seta apontando para a esquerda utilizando o caractere menor que (<) com altura de 5 linhas
+        System.out.println("    <");
+        System.out.println("  <");
+        System.out.println("<<<<<<<<<<<<<<<<<<<<");
+        System.out.println("  <");
+        System.out.println("    <");
+        System.out.println("--------------------------------");
+       //Desenhe um padrão de xadrez utilizando os caracteres cerquilha (#) e espaço em branco ( ) com tamanho de
+       //8x8
+        System.out.println(" # # # # ");
+        System.out.println("  # # # #");
+        System.out.println(" # # # #");
+        System.out.println("  # # # #");
+        System.out.println(" # # # #");
+        System.out.println("  # # # #");
+        System.out.println(" # # # #");
+        System.out.println("  # # # #");
+        System.out.println("--------------------------------");
+        //Desenhe um triângulo invertido utilizando o caractere asterisco (*) com altura de 7 linhas
+        System.out.println("   ******************");
+        System.out.println("    *              *");
+        System.out.println("     *            *");
+        System.out.println("      *          *");
+        System.out.println("        *       *");
+        System.out.println("         *     *");
+        System.out.println("          *   *");
+        System.out.println("           * *");
+        System.out.println("            *");
+        System.out.println("--------------------------------");
+
+     
+
+
+
+    }
+}
